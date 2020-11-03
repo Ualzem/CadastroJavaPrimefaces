c02eefc3513ddb4046a431e631ecf14a74c0e4ec
Ualzem <arquivosualzem@gmail.com>
2020-11-03 16:27:06 -0300

    commit inicial

---
+++ src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -0,0 +1,13 @@
+package posjavamavenhibernate;
+
+import org.junit.Test;
+
+
+
+public class TesteHibernate {
+	@Test
+	
+	public void testeHibernateUtil() {
+		HibernateUtil.getEntityManager();
+	} 	
+}
